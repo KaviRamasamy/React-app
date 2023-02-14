022c1ef851dfb70adf5c4a6cacd162c0c66bf12e
Kaviraj <kaviraj@whatarage.com>
2023-02-14 17:55:17 +0530

    kil

--- app/Frontend/src/components/AddUser.js
+++ app/Frontend/src/components/AddUser.js
@@ -18,7 +18,7 @@ const AddUser = () => {
       }, {
         headers: { 
           'Access-Control-Allow-Origin' : '*',
-          'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+          'Access-Control-Allow-Methods':('GET','PUT','POST','DELETE','PATCH','OPTIONS'),
         }
       });
       navigate("/");
